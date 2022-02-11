8ceb04fed5507269ea700a80ebb2ee2508a05a23
leesg0101 <leesg0101@gmail.com>
2022-02-11 21:17:24 +0900

    레이아웃 구성

---
+++ app/src/main/java/com/sg0101/app/qrcodereader/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.sg0101.app.qrcodereader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
