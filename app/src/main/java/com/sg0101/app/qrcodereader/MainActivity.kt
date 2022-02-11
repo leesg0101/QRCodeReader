a5dfc129491e6b7963b190dfde242dd7bef1830e
leesg0101 <leesg0101@gmail.com>
2022-02-11 20:53:15 +0900

    뷰 바인딩 적용

--- app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sg0101.app.qrcodereader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sg0101.app.qrcodereader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
