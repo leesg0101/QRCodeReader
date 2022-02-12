cd1e4e38deae4ea493e2e1f0aca62f5cd0b64456
leesg0101 <leesg0101@gmail.com>
2022-02-12 18:39:33 +0900

    QR코드 인식 기능 구현

--- app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
 import com.sg0101.app.qrcodereader.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,10 +28,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        setContentView(binding.root)
-
-        if(!hasPermissions(this)) {
+        if (!hasPermissions(this)) {
             requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
         } else {
             startCamera()
@@ -41,9 +44,10 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == PERMISSIONS_REQUEST_CODE) {
-            if(PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
                 Toast.makeText(this, "권한 요청이 승인되었습니다.", Toast.LENGTH_LONG).show()
+                startCamera()
             } else {
                 Toast.makeText(this, "권한 요청이 거부되었습니다.", Toast.LENGTH_LONG).show()
                 finish()
@@ -61,16 +65,31 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider = cameraProviderFuture.get()
             val preview = getPreview()
+            val imageAnalysis = getImageAnalysis()
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
         }, ContextCompat.getMainExecutor(this))
     }
 
-    fun getPreview() : Preview {
+    fun getPreview(): Preview {
         val preview = Preview.Builder().build()
         preview.setSurfaceProvider(binding.barcodePreview.surfaceProvider)
 
         return preview
+    }
+
+    fun getImageAnalysis(): ImageAnalysis {
+        val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+        val imageAnalysis = ImageAnalysis.Builder().build()
+
+        imageAnalysis.setAnalyzer(cameraExecutor,
+        QRCodeAnalyzer(object : OnDetectListener {
+            override fun onDetect(msg: String) {
+                Toast.makeText(this@MainActivity, "${msg}", Toast.LENGTH_SHORT).show()
+            }
+        }))
+
+        return imageAnalysis
     }
 }
