d25be7cfe244229f3609fd1f9354993afcfbb85d
leesg0101 <leesg0101@gmail.com>
2022-02-12 17:53:56 +0900

    카메라 권한 승인하기

--- app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.sg0101.app.qrcodereader
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -10,6 +14,9 @@ import com.google.common.util.concurrent.ListenableFuture
 import com.sg0101.app.qrcodereader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 1
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
@@ -20,6 +27,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        if(!hasPermissions(this)) {
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            startCamera()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSIONS_REQUEST_CODE) {
+            if(PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                Toast.makeText(this, "권한 요청이 승인되었습니다.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "권한 요청이 거부되었습니다.", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+
+    private fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) ==
+                PackageManager.PERMISSION_GRANTED
     }
 
     fun startCamera() {
