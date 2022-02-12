950121d77e76527f0ae2dea82322f3e222d00491
leesg0101 <leesg0101@gmail.com>
2022-02-12 17:40:48 +0900

    카메라 미리보기화면 구현

--- app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/sg0101/app/qrcodereader/MainActivity.kt
@@ -2,11 +2,17 @@ package com.sg0101.app.qrcodereader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.sg0101.app.qrcodereader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+    }
+
+    fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = getPreview()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun getPreview() : Preview {
+        val preview = Preview.Builder().build()
+        preview.setSurfaceProvider(binding.barcodePreview.surfaceProvider)
+
+        return preview
     }
 }
